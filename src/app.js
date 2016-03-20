e351d57c7f48a5e4e6acc082c3aaff220f94ab39
Max Kimambo <max@kimambo.de>
2016-03-20 16:39:24 +0100

    clean up

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 var bvg = require('./bvg');
 var telegram = require('./telegram')();
 var _ = require('underscore');
-
-
 var lastUpdateId = 0;
 
 function poll(offset){
@@ -14,29 +12,21 @@ function poll(offset){
 
   telegram.getMe();
   telegram.getUpdates(offset).then(function(messages){
-      // console.log(messages);
-      // console.log(messages.result);
 
       if (messages.result.length > 0){
         var result = messages.result;
 
         result.forEach(function(item){
 
-          // console.log(item.message.chat.id);
-
           var chatId = item.message.chat.id;
           var messageId = item.message.message_id;
-
-          console.log(messageId);
-// reply_to_message_id: messageId
+          // reply_to_message_id: messageId
           var message = {
             chat_id: chatId,
             text: "Some say .. I am a bot m#$%$ker"
 
           }
             lastUpdateId = item.update_id;
-            //
-          telegram.sendMessage(message);
 
         });
       }
@@ -45,7 +35,5 @@ function poll(offset){
 }
 
 setInterval(function(){
-
-  console.log('last udpate id %s', lastUpdateId);
   poll(lastUpdateId);
 }, 10000);
